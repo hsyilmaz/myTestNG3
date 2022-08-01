324081079b0d0fe947dff4c82a3b2a84dbd11d40
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-08-01 19:25:46 +0300

    Initial commit

---
+++ src/Gun01/_01_giris.java
@@ -0,0 +1,36 @@
+package Gun01;
+
+import org.testng.annotations.Test;
+
+public class _01_giris {
+//    public static void main(String[] args) {
+//        webSitesiniAc();
+//        LoginTestIsleminiYap();
+//        driverıKapat();
+//    }
+
+
+    @Test
+    void websitesiniAc(){
+        System.out.println("Driver tanimlandi ve web sitesi acildi");
+
+
+    }
+
+    @Test
+    void LoginTestIsleminiYap(){
+        System.out.println("Login test islemleri yapildi");
+
+
+    }
+
+    @Test
+    void driveriKapat(){
+        System.out.println("driver kapatildi ve cikildi");
+
+
+    }
+
+
+
+}
