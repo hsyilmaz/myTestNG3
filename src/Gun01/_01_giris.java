ffee67af0a7cc3d82bbb30b60a693392ad663554
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-08-01 19:36:23 +0300

    ilk

--- src/Gun01/_01_giris.java
+++ src/Gun01/_01_giris.java
@@ -10,21 +10,22 @@ public class _01_giris {
 //    }
 
 
-    @Test
+    @Test(priority=1)  //annotation lar
     void websitesiniAc(){
         System.out.println("Driver tanimlandi ve web sitesi acildi");
 
 
     }
 
-    @Test
+    @Test(priority=2)
     void LoginTestIsleminiYap(){
         System.out.println("Login test islemleri yapildi");
+        ///yesil tik hatali assertion yok demektir.
 
 
     }
 
-    @Test
+    @Test(priority=3)
     void driveriKapat(){
         System.out.println("driver kapatildi ve cikildi");
 
