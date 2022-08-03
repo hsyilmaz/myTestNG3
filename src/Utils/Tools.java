21417f980dba1b510eff357ca4241117814f8fa0
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-08-03 19:29:04 +0300

    .......

--- src/Utils/Tools.java
+++ src/Utils/Tools.java
@@ -23,11 +23,14 @@ public class Tools {
     }
     public static void Bekle(int saniye)
     {
-        try {
-            Thread.sleep(saniye*1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        try
+        {
+            Thread.sleep(saniye*1000);}
+
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);}
+
     }
 
 
