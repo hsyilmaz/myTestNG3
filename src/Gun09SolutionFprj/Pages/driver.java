f172264c7210facd9cebad2506fb0ba483ea0cf9
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-08-17 13:15:20 +0300

    addition

---
+++ src/Gun09SolutionFprj/Pages/driver.java
@@ -0,0 +1,51 @@
+package Gun09SolutionFprj.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class driver {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void BaslangicIslemleri(){
+        System.out.println("Driver start ....");
+
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        wait=new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30)); // sadece ana sayfa yüklenirken en başta
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.navigate().to("http://demowebshop.tricentis.com/login");
+        driver.get("http://demowebshop.tricentis.com/login");
+
+    }
+
+    @AfterClass
+    public void BitisIslemleri(){
+        System.out.println("Driver close ....");
+        try {
+            Thread.sleep(3000); }
+
+        catch (InterruptedException e) {
+            throw new RuntimeException(e); }
+        driver.quit();
+    }
+
+}
